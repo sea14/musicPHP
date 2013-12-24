e184ef38c4ea598e8bc8e202489b6516dbf8e267
sea14 <pianocat14@gmail.com>
2013-12-24 16:21:53 -0500

    started index.php script, more css

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+
+<head>
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<title>Music Organizer</title>
+</head>
+
+<body>
+
+</body>
+
+<?php
+
+	//get our db connection script
+	require '../../dbconnect.php';
+	
+
+?>
+
+
+<div id = "container">
+
+	<form id="search">
+		<input type = "text" id = "search">
+		<input type = "submit" id = "submit">
+	</form>
+
+
+</div>
+
+</html>
